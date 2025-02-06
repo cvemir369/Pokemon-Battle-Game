c4c8dcdac65801bb1e2195606597c7ad4ba1d56e
Kate Yakovleva <yakovleva492@gmail.com>
2025-02-06 10:07:27 +0100

    fixed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; 
-import PokemonList from "./components/PokemonList"; 
-import PokemonDetails from "./pages/PokemonDetails"; 
-import PokemonRoster from "./pages/PokemonRoster"; 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import PokemonList from "./components/PokemonList";
+import PokemonDetails from "./pages/PokemonDetails";
+import PokemonRoster from "./pages/PokemonRoster";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Leaderboard from "./pages/Leaderboard";
-import Details from "./pages/Details";
 import PokemonBattle from "./pages/PokemonBattle";
-
 
 const App = () => {
   return (
     <Router>
+      <Header />
 
       <nav>
         <ul>
@@ -23,26 +21,23 @@ const App = () => {
           </li>
           <li>
             <Link to="/roster">My Roster</Link>
-          </li>{" "}
-         
+          </li>
+          <li>
+            <Link to="/leaderboard">Leaderboard</Link>
+          </li>
+          <li>
+            <Link to="/battle">Battle</Link>
+          </li>
         </ul>
       </nav>
 
       <Routes>
-        <Route path="/" element={<PokemonList />} />{" "}
-      
-        <Route path="/pokemon/:name" element={<PokemonDetails />} />{" "}
-       
-        <Route path="/roster" element={<PokemonRoster />} />{" "}
-      
-
-      <Header />
-      <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/details/:id" element={<Details />} /> */}
+        <Route path="/pokemon" element={<PokemonList />} />
+        <Route path="/pokemon/:name" element={<PokemonDetails />} />
+        <Route path="/roster" element={<PokemonRoster />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/battle" element={<PokemonBattle />} />
-
       </Routes>
     </Router>
   );
